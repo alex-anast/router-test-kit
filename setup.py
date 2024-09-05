db5befd5c2c2f4e5dbe6f46cf6d851790c2a7e3b
alex-anast <anastasioyaa@gmail.com>
2024-09-05 17:20:19 +0300

    increment versio no

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="router-test-kit",
-    version="0.1.0",
+    version="0.1.1",
     author="Alexandros Anastasiou, alex-anast",
     author_email="anastasioyaa@gmail.com",
     description="A framework for remotely testing routers, physical or virtual.",
